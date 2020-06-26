a5dbaf2ffb3efc1bb8174c22f36dd3751df4a731
kaza514 <63160591+kaza514@users.noreply.github.com>
2020-06-26 19:33:15 +0530

    Update Install-Kubernetes.sh

--- Install-Kubernetes.sh
+++ Install-Kubernetes.sh
@@ -34,13 +34,14 @@ getSubDomain() {
 
 installKubectl() {
         curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
-        curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.7.0/bin/linux/amd64/kubectl
+        #curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.7.0/bin/linux/amd64/kubectl
         chmod +x ./kubectl
         sudo mv ./kubectl /usr/local/bin/kubectl
 }
 
 installKops() {
-        curl -L0 https://github.com/kubernetes/kops/releases/download/1.6.1/kops-linux-amd64
+        curl -LO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+        #curl -L0 https://github.com/kubernetes/kops/releases/download/1.6.1/kops-linux-amd64
         chmod +x kops-linux-amd64
         sudo mv kops-linux-amd64 /usr/local/bin/kops
 }
